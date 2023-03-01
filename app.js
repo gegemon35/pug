57648b32f24cc3e97af023718c67f11141435d95
Maks Nikiforov aka gega <124780586+gegemon35@users.noreply.github.com>
2023-03-01 15:49:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const host = '127.0.0.1'
+const port = 7000
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.use(express.static(__dirname + "/style"));
+
+app.get('/', (req, res) => {
+  res.render('main', { title: 'Greetings from Pug' })
+})
+app.get('/index', (req, res) => {
+  res.render('index', { title: 'Greetings from Pug' })
+})
+app.get('/home', (req, res) => {
+  res.render('home', { title: 'Greetings from Pug' })
+})
+
+app.listen(port, host, function () {
+  console.log(`Server listens http://${host}:${port}`)
+})
+app.get('/', (req, res) => {
+  res.render('home')
+})
